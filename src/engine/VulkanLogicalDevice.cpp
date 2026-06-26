4e0257df0dd1f955ee750cd36d76779f47770721
Jose Luis Nunez <8852923+jonunezg@users.noreply.github.com>
2026-06-26 07:35:17 -0700

    Create device

--- src/engine/VulkanLogicalDevice.cpp
+++ src/engine/VulkanLogicalDevice.cpp
@@ -1,3 +1,4 @@
+#include "VulkanLayers.h"
 #include "VulkanLogicalDevice.h"
 
 VulkanLogicalDevice::VulkanLogicalDevice(const std::shared_ptr<VulkanPhysicalDevice> physicalDevice) :
@@ -17,16 +18,30 @@ m_physicalDevice { std::move(physicalDevice) }
         .flags = 0,
         .queueFamilyIndex = m_physicalDevice->getSelectedDevice().graphicQueueIndex.value(),
         .queueCount = 1,
-        .pQueuePriorities = &priority
+        .pQueuePriorities = &priority,
     };
 
     const VkPhysicalDeviceFeatures deviceFeatures {};
 
+    const VkDeviceCreateInfo createInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .queueCreateInfoCount = 1,
+        .pQueueCreateInfos = &queueCreateInfo,
+        .enabledLayerCount = 0,
+        .ppEnabledLayerNames = nullptr,
+        .enabledExtensionCount = 0,
+        .ppEnabledExtensionNames = nullptr,
+        .pEnabledFeatures = &deviceFeatures,
+    };
 
-
+    VK_TERMINATE_IF_FAILED(vkCreateDevice(m_physicalDevice->getSelectedDevice().device, &createInfo, nullptr, &m_device));
 }
 
 VulkanLogicalDevice::~VulkanLogicalDevice()
 {
+    vkDestroyDevice(m_device, nullptr);
     std::cout << "Vulkan logical device destroyed" << std::endl;
 }
