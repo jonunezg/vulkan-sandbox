a7c3b5a36e9650b7af26d3a91ce166cdfd582502
Jose Luis Nunez <8852923+jonunezg@users.noreply.github.com>
2026-08-02 13:07:11 -0700

    Rasterization and multisampling structs

--- src/engine/pipeline/VulkanPipelineManager.cpp
+++ src/engine/pipeline/VulkanPipelineManager.cpp
@@ -2,7 +2,7 @@
 
 VulkanPipelineManager::VulkanPipelineManager()
 {
-    VkPipelineVertexInputStateCreateInfo vertexInputInfo
+    VkPipelineVertexInputStateCreateInfo vertexInputCreateInfo
     {
         .sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO,
         .pNext = nullptr,
@@ -13,7 +13,7 @@ VulkanPipelineManager::VulkanPipelineManager()
         .pVertexAttributeDescriptions = nullptr,
     };
 
-    VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo
+    VkPipelineInputAssemblyStateCreateInfo inputAssemblyCreateInfo
     {
         .sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
         .pNext = nullptr,
@@ -22,6 +22,35 @@ VulkanPipelineManager::VulkanPipelineManager()
         .primitiveRestartEnable = VK_FALSE,
     };
 
+    VkPipelineRasterizationStateCreateInfo rasterizationStateCreateInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .depthClampEnable = VK_FALSE,
+        .rasterizerDiscardEnable = VK_FALSE,
+        .polygonMode = VK_POLYGON_MODE_FILL,
+        .cullMode = VK_CULL_MODE_BACK_BIT,
+        .frontFace = VK_FRONT_FACE_CLOCKWISE,
+        .depthBiasEnable = VK_FALSE,
+        .depthBiasConstantFactor = 0.0f,
+        .depthBiasClamp = 0.0f,
+        .depthBiasSlopeFactor = 0.0f,
+        .lineWidth = 1.0f,
+    };
+
+    VkPipelineMultisampleStateCreateInfo multisampleStateCreateInfo
+    {
+        .sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+        .pNext = nullptr,
+        .flags = 0,
+        .rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+        .sampleShadingEnable = VK_FALSE,
+        .minSampleShading = 1.0f,
+        .pSampleMask = nullptr,
+        .alphaToCoverageEnable = VK_FALSE,
+        .alphaToOneEnable = VK_FALSE,
+    };
 }
 
 VulkanPipelineManager::~VulkanPipelineManager()
