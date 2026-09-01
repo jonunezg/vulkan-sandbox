e85ce60ac95b26f6547f966a97ef7d31d4f99476
Jose Luis Nunez <8852923+jonunezg@users.noreply.github.com>
2026-08-31 17:35:11 -0700

    Remove unnecessary comments

--- src/engine/pipeline/include/VulkanBuffer.h
+++ src/engine/pipeline/include/VulkanBuffer.h
@@ -13,30 +13,9 @@ class VulkanBuffer
 public:
     VulkanBuffer(const PhysicalDevice& physicalDevice, const VkDevice& device, const std::vector<Geometry::Vertex>& vertices);
 
-    // VulkanBuffer(VulkanBuffer& other) :
-    // m_physicalDevice { other.m_physicalDevice },
-    // m_device { other.m_device }
-    // {
-    //     m_vertexBuffer = other.m_vertexBuffer;
-    //     other.m_vertexBuffer = nullptr;
-
-    //     m_vertexBufferMemory = other.m_vertexBufferMemory;
-    //     other.m_vertexBufferMemory = nullptr;
-    // }
-    
+    VulkanBuffer(VulkanBuffer& other) = delete;
     VulkanBuffer(const VulkanBuffer&) = delete;
-
-    // VulkanBuffer(VulkanBuffer&& other) :
-    // m_physicalDevice { other.m_physicalDevice },
-    // m_device { other.m_device }
-    // {
-    //     m_vertexBuffer = other.m_vertexBuffer;
-    //     other.m_vertexBuffer = nullptr;
-
-    //     m_vertexBufferMemory = other.m_vertexBufferMemory;
-    //     other.m_vertexBufferMemory = nullptr;
-    // }
-
+    VulkanBuffer(VulkanBuffer&& other) = delete;
     VulkanBuffer(const VulkanBuffer&&) = delete;
 
     ~VulkanBuffer();
